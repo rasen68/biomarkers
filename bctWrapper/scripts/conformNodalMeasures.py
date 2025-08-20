d9fb877a280999463fe6484dc02348ca4acb1986
triowave <oliviaforman2023@gmail.com>
2025-08-20 00:36:09 -0400

    I added the command to run it

--- bctWrapper/scripts/conformNodalMeasures.py
+++ bctWrapper/scripts/conformNodalMeasures.py
@@ -20,6 +20,10 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
 def trainModel(X, y):
+
+    # RUN THIS COMMAND
+    # python .\conformNodalMeasures.py ..\features\ .\nodalMeasuresMatrices\subjects.txt .\nodalMeasuresMatrices\adosScores.txt
+
     # Split the dataset into training (80%) and testing (20%) sets.
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -77,6 +81,7 @@ if __name__ == "__main__":
     meaningfulResults = []
 
     for measure in graphTheoryMeaures:
+        if (measure != "eccentricity"): continue
         featurePath = f'{features}{measure}.txt'
         if os.path.exists(featurePath):
             for node in nodes_of_interest:
